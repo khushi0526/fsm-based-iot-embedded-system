88505b4e6f7498e93e6b2b8544a86c447f5ad5be
KHUSHI B S <bskhushi2@gmail.com>
2026-03-02 23:54:59 +0530

    Day 1: Basic GPIO button-LED control
    Add button control for LED in main.ino

--- code/main.ino
+++ code/main.ino
@@ -1,1 +1,18 @@
+const int buttonPin = 4;
+const int ledPin = 2;
 
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int buttonState = digitalRead(buttonPin);
+
+  if (buttonState == LOW) {   
+    // Button pressed
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
