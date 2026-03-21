5e1504aea95455774e45778fd231333af03fd63b
KHUSHI B S <bskhushi2@gmail.com>
2026-03-21 16:25:25 +0530

    Create main.ino

---
+++ code/Day25/main.ino
@@ -0,0 +1,38 @@
+const int sensorPin = 34;   // Analog input
+const int ledPin = 2;
+const int buzzerPin = 4;
+
+int value;
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+
+  value = analogRead(sensorPin);
+
+  Serial.print("Power Value: ");
+  Serial.println(value);
+
+  if (value < 1500) {
+    // LOW usage
+    digitalWrite(ledPin, LOW);
+    digitalWrite(buzzerPin, LOW);
+  }
+  else if (value < 3000) {
+    // MEDIUM usage
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(buzzerPin, LOW);
+  }
+  else {
+    // HIGH usage
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(buzzerPin, HIGH);
+  }
+
+  delay(500);
+}
