8e011188b303d2e76182807df9a802d2253b2173
KHUSHI B S <bskhushi2@gmail.com>
2026-05-05 23:57:52 +0530

    Remove power usage comments from main.ino
    Removed comments indicating power usage levels in the loop.

--- code/Day25/main.ino
+++ code/Day25/main.ino
@@ -1,12 +1,10 @@
-const int sensorPin = 34;   // Analog input
+const int sensorPin = 34;   
 const int ledPin = 2;
 const int buzzerPin = 4;
-
 int value;
 
 void setup() {
   Serial.begin(115200);
-
   pinMode(ledPin, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
 }
@@ -14,22 +12,18 @@ void setup() {
 void loop() {
 
   value = analogRead(sensorPin);
-
   Serial.print("Power Value: ");
   Serial.println(value);
 
   if (value < 1500) {
-    // LOW usage
     digitalWrite(ledPin, LOW);
     digitalWrite(buzzerPin, LOW);
   }
   else if (value < 3000) {
-    // MEDIUM usage
     digitalWrite(ledPin, HIGH);
     digitalWrite(buzzerPin, LOW);
   }
   else {
-    // HIGH usage
     digitalWrite(ledPin, HIGH);
     digitalWrite(buzzerPin, HIGH);
   }
